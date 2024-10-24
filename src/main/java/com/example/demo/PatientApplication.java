efbf84a22be1f27c096201a8ef825bd13f2d1bdd
devi252 <k.naveen252@gmail.com>
2024-10-24 17:50:45 +0530

    no status for commit

--- src/main/java/com/example/demo/PatientApplication.java
+++ src/main/java/com/example/demo/PatientApplication.java
@@ -10,7 +10,7 @@ public class PatientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
-		System.out.println("app running....");
+		System.out.println("app started....");
 	}
 
 }
